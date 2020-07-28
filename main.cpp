96924e66f7912af1f555593298845a4616d001d1
Ilya Prostakov <vipcow337@gmail.com>
2020-07-29 05:14:16 +0600

    Add: class Player, Gravitation

--- main.cpp
+++ main.cpp
@@ -1,90 +1,106 @@
 #include <SFML/Graphics.hpp>
-using namespace sf;
+using namespace sf;// Так как мы работаем с библиотекой sfml, нам будет легче не обращаться каждый раз к 
+                   //определенному классу, так что используем пространство имен sf
+
+int ground = 150;  // Высота на которой расположена земля
+
+class Player // Класс игрока
+{
+    FloatRect rect;                         // Прямоугольник, задающий позицию картинки sprite
+    float frameChangeVelosity = 0.00001;    // Скорость смены кадра
+    float currentFrame;                     // Переменная считающая кадр который будет в анимации
+    enum MoveDirection { LEFT, RIGHT, UP, DOWN };
+    MoveDirection lastDir;
+public: 
+    float dx, dy;                           // Скорость по двум осям          
+    bool onGround;                          // Ответ на вопрос: На земле ли мы?
+    Sprite sprite;                          // Спрайт
+    
+
+    Player(Texture& image)                  //Класс будет принимать параметром спрайт
+    {
+        sprite.setTexture(image);   
+        sprite.setTextureRect(IntRect(0, 0, 32, 32));
+        rect = FloatRect(0, 0, 32, 32);    
+        
+
+        lastDir = MoveDirection::RIGHT;
+        onGround = false;
+        dx = dy = 0;
+        currentFrame = 0;
+    }
+
+    void update(float time)
+    {
+        rect.left += dx * time;                     // rect.left - позиция sprite в окне по оси ox
+        if (!onGround) dy = dy + 0.000000002 * time; 
+
+        rect.top += dy * time;                      // rect.top - позиция sprite в окне по оси oy
+
+        if (rect.top > ground) { rect.top = ground; dy = 0; onGround = true; }
+
+        currentFrame += frameChangeVelosity * time; // Двигаясь по спрайт листу мы меняем кадры в зависимости от скорости
+        if (currentFrame > 8) currentFrame -= 8;
+
+        if (dx == 0)                                // В зависимости от скорости изменяем анимацию         
+            switch (lastDir) 
+            { 
+                case MoveDirection::LEFT:  sprite.setTextureRect(IntRect(32, 0, -32, 32)); break;
+                case MoveDirection::RIGHT: sprite.setTextureRect(IntRect(0, 0, 32, 32)); break;
+            }
+        if (dx > 0) 
+        {
+            sprite.setTextureRect(IntRect(32 * int(currentFrame), 32, 32, 32));
+            lastDir = MoveDirection::RIGHT;
+        }
+        if (dx < 0)
+        {
+            sprite.setTextureRect(IntRect(32 + 32 * int(currentFrame), 32, -32, 32));
+            lastDir = MoveDirection::LEFT;;
+        }
+
+        sprite.setPosition(rect.left, rect.top);
+
+        dx = 0;
+    }
+};
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!"); //Рендерим окно
-    Texture t;                                                       //Инициализируем текстуру
-    t.loadFromFile("man.png");                                       //Загружаем текстуру из файла
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    Texture t;
+    t.loadFromFile("man.png");
 
-    Sprite s;                                       // Инициализируем спрайт = загруженной текстуре взятой в\
-                                                        определенном месте картинки
-    s.setTexture(t);                                // Устанавливаем текстуру спрайта
-    s.setTextureRect(IntRect(0, 0, 32, 32));        // Устанавливаем Rect - прямоугольную часть текстуры
-    s.setPosition(0, 0);                            // Устанавливаем спрайт в определенное место в окне
+    Player man(t);
 
-    int lastKeyPressed = Keyboard::Right;// Последняя нажатая клавиша
-    float currentFrame = 0;              // Переменная считающая кадр который будет в анимации. 
-    float moveVelosity = 0.0001;         // Скорость движения персонажа
-    float spriteFrameVelosity = 0.00001; // Скорость смены кадра
-    Clock clock;                         // Переменная класса часы, считающее время
-    
+    Clock clock;
     
     while (window.isOpen()) 
     {
         float time = clock.getElapsedTime().asMicroseconds();//
         clock.restart();
         Event event;
-        while (window.pollEvent(event))       // Эта функция ожидает событие типа Закрытие 
-        {                                    //и закрывает окно если оно произошло
+        if (window.pollEvent(event))       // window.pollEvent(event) ожидает событие типа Закрытие и закрывает окно если оно произошло
+        {                                    
             if (event.type == Event::Closed)
                 window.close();
         }
         if (Keyboard::isKeyPressed(Keyboard::Left)) 
         {
-            lastKeyPressed = Keyboard::Left;
-
-            s.move(-moveVelosity * time, 0); //Двигаем персонажа влево, с определенной скоростью
-                                             //Note: эта скорость одинакова на всех компьютерах, т.к зависит не
-                                             // от скорости компьютера, а от времени. 
-                                             // Те если бы мы оставили просто функцию c параметром moveVelocity, то
-                                             // функция срабатывала бы, с такой скоростью, с какой работает цикл while
-                                             // скорость которого зависит только от быстродействия ПК
-
-            currentFrame += spriteFrameVelosity * time; //Так-же изменяем sprite персонажа, в зависимости
-            if (currentFrame > 8) currentFrame -= 8;    // от переменной currentFrame, простым изменением позиции Rect.
-            s.setTextureRect(IntRect(32 + 32 * int(currentFrame), 32, -32, 32));
+            man.dx += -0.0002;
         }
         if (Keyboard::isKeyPressed(Keyboard::Right))
         {
-            lastKeyPressed = Keyboard::Right;
-
-            s.move(moveVelosity * time, 0);
-
-            currentFrame += spriteFrameVelosity * time;
-            if (currentFrame > 8) currentFrame -= 8;
-            s.setTextureRect(IntRect(32 * int(currentFrame), 32, 32, 32));
-
-
+            man.dx += 0.0002;
         }
-        if (Keyboard::isKeyPressed(Keyboard::Down))
-        {
-            s.move(0, moveVelosity * time);
-        }
+
         if (Keyboard::isKeyPressed(Keyboard::Up))
         {
-            s.move(0, -moveVelosity * time);
+            if (man.onGround) { man.dy = -0.0005; man.onGround = false; }
         }
-        window.clear();
-        window.draw(s);
-        window.display();
-        if (!
-            (
-                Keyboard::isKeyPressed(Keyboard::Left) ||
-                Keyboard::isKeyPressed(Keyboard::Right) ||
-                Keyboard::isKeyPressed(Keyboard::Up) ||
-                Keyboard::isKeyPressed(Keyboard::Down)
-                )
-            )
-        {
-            currentFrame = 0;
-            if (lastKeyPressed == Keyboard::Left)
-                s.setTextureRect(IntRect(32, 0, -32, 32));
-            else
-                s.setTextureRect(IntRect(0, 0, 32, 32));
-        }
+        man.update(time);
         window.clear();  //Очищаем окно
-        window.draw(s);  //Рисуем спрайт
+        window.draw(man.sprite);  //Рисуем спрайт
         window.display();//Выводим спрайт на экран
     }
 
